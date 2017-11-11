d10faf5ce3fec27fa2f18d4bc97a56618d470e94
Stefan Scheidt <stefan.scheidt@icloud.com>
2017-11-12 00:25:44 +0100

    baseline for JUnit 5.

--- src/main/kotlin/gol/Coordinate.kt
+++
@@ -1,7 +0,0 @@
-package gol
-
-fun pos(x: Int, y: Int) = Coordinate(x, y)
-
-data class Coordinate(val x: Int, val y: Int) {
-    override fun toString() = "($x, $y)"
-}
--- src/test/kotlin/gol/GamoOfLifeTest.kt
+++ src/test/kotlin/gol/GamoOfLifeTest.kt
@@ -1,7 +1,5 @@
 package gol
 
-import assertk.assert
-import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
@@ -9,15 +7,9 @@ import org.junit.jupiter.api.Test
 class GamoOfLifeTest {
 
     @Test
-    @DisplayName("empty world should stay empty")
-    fun emptyWorld() {
-        assert(emptyWorld.tick()).isEqualTo(emptyWorld)
-    }
-
-    @Test
-    @DisplayName("block should stay block")
-    fun block() {
-        assert(block.tick()).isEqualTo(block)
+    @DisplayName("replace with first test")
+    fun firstTest() {
+        TODO("implement first test")
     }
 
 }
